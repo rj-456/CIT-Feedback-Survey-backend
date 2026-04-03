7f6b02abee8cecd2cc35eccd82206a0f2cf75a55
rj-456 <santosrancis04@gmail.com>
2026-04-03 20:00:42 +0800

    Updated database schema to make name and email optional

--- models/Feedback.js
+++ models/Feedback.js
@@ -4,14 +4,19 @@ const feedbackSchema = new mongoose.Schema({
     // Encrypted Identification
     classification: { type: String, required: true }, 
     classificationIv: { type: String, required: true },
-    name: { type: String, required: true }, 
-    nameIv: { type: String, required: true },
+    
+    // Made optional
+    name: { type: String, required: false }, 
+    nameIv: { type: String, required: false },
     
     // Encrypted Data
     rating: { type: String, required: true }, 
     ratingIv: { type: String, required: true },
-    encryptedEmail: { type: String, required: true },
-    emailIv: { type: String, required: true },
+    
+    // Made optional
+    encryptedEmail: { type: String, required: false },
+    emailIv: { type: String, required: false },
+    
     encryptedFeedback: { type: String, required: true },
     feedbackIv: { type: String, required: true },
     
